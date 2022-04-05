ee7988c8f50138685f6a12d4e06c55eed9ab877f
Martin Piatka <piatka@cesnet.cz>
2022-04-05 13:42:53 +0200

    VideoDisplay: Fix viewport transform for aspect ratios != 2

--- Assets/Scripts/Video/VideoDisplayComponent.cs
+++ Assets/Scripts/Video/VideoDisplayComponent.cs
@@ -24,6 +24,7 @@ public class VideoDisplayComponent : MonoBehaviour
     public void Resize(VideoScene.VideoAspectRatioEnum aspectRatio)
     {
         var viewport = RigSpawner.CameraRig.GetBoundaries(5);
+        var viewportAspect = viewport.width / (float)viewport.height;
         var videoAspect = VideoPlayer.width / (float)VideoPlayer.height;
 
         // 10m is the default unity plane size, why it's not 1 is beyond me...
@@ -32,41 +33,21 @@ public class VideoDisplayComponent : MonoBehaviour
         var widthFit = new Vector3(viewport.width / planeSize, 1, (viewport.width / videoAspect) / planeSize);
         var heightFit = new Vector3((viewport.height * videoAspect) / planeSize, 1, viewport.height / planeSize);
 
-        if (RigSpawner.CameraRig.Orientation == IPWSetting.IPWOrientation.Horizontal)
+        switch (aspectRatio)
         {
-            switch (aspectRatio)
-            {
-                case VideoScene.VideoAspectRatioEnum.Stretch:
-                    transform.localScale = new Vector3(viewport.width / planeSize, 1, viewport.height / planeSize);
-                    break;
-                case VideoScene.VideoAspectRatioEnum.FitInside:
-                    transform.localScale = videoAspect > 2 ? widthFit : heightFit;
-                    break;
-                case VideoScene.VideoAspectRatioEnum.FitOutside:
-                    transform.localScale = videoAspect > 2 ? heightFit : widthFit;
-                    break;
-                default:
-                    throw new NotSupportedException();
-            }
+            case VideoScene.VideoAspectRatioEnum.Stretch:
+                transform.localScale = new Vector3(viewport.width / planeSize, 1, viewport.height / planeSize);
+                break;
+            case VideoScene.VideoAspectRatioEnum.FitInside:
+                transform.localScale = videoAspect > viewportAspect ? widthFit : heightFit;
+                break;
+            case VideoScene.VideoAspectRatioEnum.FitOutside:
+                transform.localScale = videoAspect > viewportAspect ? heightFit : widthFit;
+                break;
+            default:
+                throw new NotSupportedException();
         }
-        else
-        {
-            switch (aspectRatio)
-            {
-                case VideoScene.VideoAspectRatioEnum.Stretch:
-                    transform.localScale = new Vector3(viewport.width / planeSize, 1, viewport.height / planeSize);
-                    break;
-                case VideoScene.VideoAspectRatioEnum.FitInside:
-                    transform.localScale = videoAspect < 2 ? widthFit : heightFit;
-                    break;
-                case VideoScene.VideoAspectRatioEnum.FitOutside:
-                    transform.localScale = videoAspect < 2 ? heightFit : widthFit;
-                    break;
-                default:
-                    throw new NotSupportedException();
-            }
-        }
-        
+
         transform.localPosition = new Vector3(viewport.center.x, viewport.center.y, 5);
     }
 }
